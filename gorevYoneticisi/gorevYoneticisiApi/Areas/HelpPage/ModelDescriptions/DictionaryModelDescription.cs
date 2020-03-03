034775e331b11e565b6b4f8508638ac19a3da8f8
“Burak <burakcetin9724@gmail.com>
2020-03-03 20:53:46 +0200

    v1

---
+++ gorevYoneticisi/gorevYoneticisiApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace gorevYoneticisiApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ gorevYoneticisi/gorevYoneticisiApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gorevYoneticisiApi.WebApiApplication" Language="C#" %>
